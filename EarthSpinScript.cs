29eb7646c5d21debe16b5799ae79d2d1d536b5a0
Kiruthika111 <62352607+Kiruthika111@users.noreply.github.com>
2020-03-23 10:40:39 +0530

    Add files via upload

---
+++ EarthSpinScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpinScript : MonoBehaviour {
+    
+
+    void Update() {
+        transform.Rotate(new Vector3(0f, 1f, 0f));
+    }
+}
---
+++ revolve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revolve : MonoBehaviour
+{
+    public GameObject orbitAround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbitAround=GameObject.Find("sun");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(orbitAround.transform.position,Vector3.down,0.3f);
+    }
+}
